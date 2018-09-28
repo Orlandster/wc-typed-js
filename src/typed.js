525f2a1ba9f7ed761e8b1cf97b1ad4b79c3e7ab3
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2018-09-28 23:17:59 +0200

    [Feature]: Add TypedJS properties with types

--- src/typed.js
+++ src/typed.js
@@ -7,7 +7,25 @@ class TypedJS extends LitElement {
 
     static get properties() {
         return {
-
+            strings: {type: Array},
+            stringsElement: {type: String},
+            typeSpeed: {type: Number},
+            startDelay: {type: Number},
+            backSpeed: {type: Number},
+            smartBackspace: {type: Boolean},
+            shuffle: {type: Boolean},
+            backDelay: {type: Number},
+            fadeOut: {type: Boolean},
+            fadeOutClass: {type: String},
+            fadeOutDelay: {type: Boolean},
+            loop: {type: Boolean},
+            loopCount: {type: Number},
+            showCursor: {type: Boolean},
+            cursorChar: {type: String},
+            autoInsertCss: {type: Boolean},
+            attr: {type: String},
+            bindInputFocusEvents: {type: Boolean},
+            contentType: {type: String},
         }
     }
 
